d7a71588f692c5b62b81ab7b393442215aa5f428
anohene1 <anoheneisaac@gmail.com>
2022-06-22 05:18:24 +0000

    small changes to emergency contacts screen

--- lib/view/pages/settings/emergency_contacts.dart
+++ lib/view/pages/settings/emergency_contacts.dart
@@ -51,91 +51,86 @@ class _EmergencyContactsScreenState extends State<EmergencyContactsScreen> {
               color: HexColor.fromHex("#181a1f"),
               position: "topLeft",
             ),
-            Container(
-              padding: const EdgeInsets.only(left: 20, right: 20),
-              child: StreamBuilder<QuerySnapshot>(
-                stream: database.emergencyContactsStream(),
-                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (snapshot.hasError) {
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Text('Oopsie...', style: GoogleFonts.lato(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),),
-                        Text('An error occurred: ${snapshot.error.toString()}', style: TextStyle(fontSize: 18, color: HexColor.fromHex("666A7A")))
-                      ],
-                    );
-                  }
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        CircularProgressIndicator(color: Theme.of(context).primaryColor,),
-                      ],
-                    );
-                  }
-                  if (snapshot.data!.docs.isEmpty) {
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Text('Nobody\'s here :(', style: GoogleFonts.lato(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),),
-                        Text('Add people who should be contacted in case you fall (although we hope you don\'t)', style: TextStyle(fontSize: 18, color: HexColor.fromHex("666A7A")))
-                      ],
-                    );
-                  }
-                  return ListView.builder(
-                    itemCount: snapshot.data!.docs.length,
-                    itemBuilder: (context, index) {
-                      final list = snapshot.data!.docs;
-                      DocumentSnapshot document = list[index];
-                      final data = document.data()! as Map<String, dynamic>;
-
-                      deleteContact() {
-                        database.deleteEmergencyContact(document.id);
-                        var deletedItem = list.removeAt(index).data()! as Map<String, dynamic>;
-
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text('${deletedItem['name']} is removed', style: GoogleFonts.montserrat(),),
-                              action: SnackBarAction(label: 'Undo', onPressed: () {
-                                database.addEmergencyContact(EmergencyContact(name: deletedItem['name'], number: deletedItem['number']));
-                              },
-
-                              ),
-                            )
-                        );
-                      }
-                      return Slidable(
-                          key: ValueKey(document.id),
-                          endActionPane: ActionPane(
-                            dismissible: DismissiblePane(onDismissed: deleteContact),
-                            motion: const ScrollMotion(),
-                            children: [
-                              SlidableAction(
-                                onPressed: (context) {
-                                  deleteContact();
-                                },
-                                icon: LineIcons.trash,
-                                label: 'Delete',
-                                backgroundColor: Colors.red,
-                                foregroundColor: Colors.white,
-                              )
-                            ],
-
-                          ),
-                          child: ListTile(
-                            title: Text(data['name']),
-                            subtitle: Text(data['number']),
-                          ));
-                    },
+            StreamBuilder<QuerySnapshot>(
+              stream: database.emergencyContactsStream(),
+              builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text('Oopsie...', style: GoogleFonts.lato(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),),
+                      Text('An error occurred: ${snapshot.error.toString()}', style: TextStyle(fontSize: 18, color: HexColor.fromHex("666A7A")))
+                    ],
                   );
-                },
-              )
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child:
+                      CircularProgressIndicator(color: Theme.of(context).primaryColor,),
+                  );
+                }
+                if (snapshot.data!.docs.isEmpty) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text('Nobody\'s here :(', style: GoogleFonts.lato(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),),
+                      Text('Add people who should be contacted in case you fall (although we hope you don\'t)', style: TextStyle(fontSize: 18, color: HexColor.fromHex("666A7A")))
+                    ],
+                  );
+                }
+                return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    final list = snapshot.data!.docs;
+                    DocumentSnapshot document = list[index];
+                    final data = document.data()! as Map<String, dynamic>;
+
+                    deleteContact() {
+                      database.deleteEmergencyContact(document.id);
+                      var deletedItem = list.removeAt(index).data()! as Map<String, dynamic>;
+
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            behavior: SnackBarBehavior.floating,
+                            content: Text('${deletedItem['name']} is removed', style: GoogleFonts.montserrat(),),
+                            action: SnackBarAction(label: 'Undo', onPressed: () {
+                              database.addEmergencyContact(EmergencyContact(name: deletedItem['name'], number: deletedItem['number']));
+                            },
+
+                            ),
+                          )
+                      );
+                    }
+                    return Slidable(
+                        key: ValueKey(document.id),
+                        endActionPane: ActionPane(
+                          dismissible: DismissiblePane(onDismissed: deleteContact),
+                          motion: const ScrollMotion(),
+                          children: [
+                            SlidableAction(
+                              onPressed: (context) {
+                                deleteContact();
+                              },
+                              icon: LineIcons.trash,
+                              label: 'Delete',
+                              backgroundColor: Colors.red,
+                              foregroundColor: Colors.white,
+                            )
+                          ],
+
+                        ),
+                        child: ListTile(
+                          contentPadding: const EdgeInsets.symmetric(horizontal: 40),
+                          title: Text(data['name']),
+                          subtitle: Text(data['number']),
+                        ));
+                  },
+                );
+              },
             )
           ],
         ),
