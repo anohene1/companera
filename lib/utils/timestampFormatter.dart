aa8a69cfba191a00da424e10071df12bfab2c609
anohene1 <anoheneisaac@gmail.com>
2022-08-18 13:40:06 +0000

    complete date formatter

---
+++ lib/utils/timestampFormatter.dart
@@ -0,0 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
+
+class TimestampFormatter {
+  static String format(int timestamp) {
+    DateTime date = Timestamp.fromMillisecondsSinceEpoch(timestamp).toDate();
+    String dateString = DateFormat.yMMMMEEEEd().format(date);
+    return dateString;
+  }
+}
--- lib/view/pages/fall_detection/today.dart
+++ lib/view/pages/fall_detection/today.dart
@@ -1,20 +1,66 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:companera/services/db.dart';
+import 'package:companera/utils/timestampFormatter.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../../../constants/app-colors.dart';
 
 class Today extends StatelessWidget {
-  const Today({Key? key}) : super(key: key);
+  Today({Key? key}) : super(key: key);
+
+  DB database = DB();
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text('No falls recorded!', style: GoogleFonts.lato(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),),
-        Text('You are doing very well today, no falls have been detected.', style: TextStyle(fontSize: 18, color: HexColor.fromHex("666A7A")),)
-      ],
+    return StreamBuilder<QuerySnapshot>(
+      stream: database.streamFalls(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child:
+            CircularProgressIndicator(color: Theme.of(context).primaryColor,),
+          );
+        }
+        if (snapshot.hasError) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Text('Oopsie...', style: GoogleFonts.lato(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),),
+                Text('An error occurred: ${snapshot.error.toString()}', style: TextStyle(fontSize: 18, color: HexColor.fromHex("666A7A")))
+              ],
+            ),
+          );
+        }
+        if (snapshot.data!.docs.isEmpty) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('No falls recorded!', style: GoogleFonts.lato(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),),
+              Text('You are doing very well today, no falls have been detected.', style: TextStyle(fontSize: 18, color: HexColor.fromHex("666A7A")),)
+            ],
+          );
+        }
+        return ListView.builder(
+          itemCount: snapshot.data!.docs.length,
+          itemBuilder: (context, index) {
+            final list = snapshot.data!.docs;
+            DocumentSnapshot document = list[index];
+            final data = document.data()! as Map<String, dynamic>;
+
+            return ListTile(
+              title: Text('Fall $index'),
+              subtitle: Text(TimestampFormatter.format(data['timestamp'])),
+            );
+          },
+        );
+    },
     );
   }
 }
